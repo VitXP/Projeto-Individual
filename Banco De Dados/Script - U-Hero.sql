08c4f6215af5bd51a8043b2daeb8dd31dca021db
vitXP <vitor.pereira@sptech.school>
2022-11-08 20:54:12 -0300

    Alteração no banco de dados

--- Banco De Dados/Script - U-Hero.sql
+++ Banco De Dados/Script - U-Hero.sql
@@ -5,72 +5,57 @@ create database hero;
 use hero;
 
 -- Criar tabelas
-create table Metrica (
-idMetrica int primary key auto_increment,
-voto int
+create table Funcao (
+idFuncao int primary key auto_increment,
+descricao varchar(200)
 );
 
 
 create Table Classe (
 idClasse int auto_increment,
-primary key (idClasse,fkMetrica),
+primary key (idClasse,fkFuncao),
 nomeClasse varchar(7), constraint chknomeClasse check (nomeClasse in ('suporte','dano','tanque')),
-fkMetrica int,
-foreign key (fkMetrica) references metrica (idMetrica)
+fkFuncao int,
+foreign key (fkFuncao) references funcao (idFuncao)
 );
 
 
 
-create table Cadastro (
-idCadastro int auto_increment,
-primary key (idCadastro, fkClasse),
+create table Usuario (
+idUsuario int auto_increment,
+primary key (idUsuario, fkClasse),
 nome varchar (10),
 email varchar(45), constraint chkemail check (email like '%@%'),
 senha varchar (12),
-confirmarSenha varchar (12),
 fkClasse int,
 foreign key (fkClasse) references classe (idClasse)
 );
 
-alter table cadastro rename column apelido to nome;
 -- Inserir dados
-insert into metrica values
-(1,1),
-(2,1),
-(3,1),
-(4,1);
+-- Inserir dados
+insert into Funcao values
+(1,' Heróis de suporte fortalecem seus aliados, pois são capazes de curá-los, protegê-los com escudos, aumentar seu poder de dano e imobilizar os inimigos.'),
+(2,'Heróis de dano são responsáveis por procurar, atacar e aniquilar inimigos, valendo-se de uma ampla variedade de ferramentas, habilidades e estratégias.'),
+(3,'Heróis desse tipo são especializados em proteger e dar cobertura à seus aliados, tirar o foco de inimigos e ocupar a linha de frente.');
 
 insert into classe values
 (null,'suporte',1),
 (null,'dano',2),
-(null,'tanque',3),
-(null,'suporte',1);
+(null,'tanque',3);
 
-insert into cadastro values 
-(null,'vitor','vitor.pr@gmail.com','123','123',1),
-(null,'saraxp','sara@gmail.com','123','123',2),
-(null,'clara','clara@gmail.com','123','123',1),
-(null,'alan','alan@gmail.com','123','123',3);
 
 
 -- Selecionar tabelas
-select * from cadastro;
+select * from usuario;
 select * from classe;
-select * from metrica;
+select * from funcao;
 
--- Selecionar cadastro, classe e metrica
-select*From cadastro as c join classe as cl on c.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica;
+--  Selecionar apelido, email, senha, nomeClasse e voto das tabelas cadastro, classe.
+select  u.nome, u.email as 'E-mail',u.senha, cl.nomeClasse as 'Classe', f.descricao as 'Descrição' From usuario as u join classe as cl on u.fkclasse = cl.idclasse join funcao as f on f.idfuncao = cl.fkfuncao order by u.nome;
 
---  Selecionar apelido, email, senha, confirmar senha, nomeClasse e voto das tabelas cadastro, classe e metrica.
-select c.nome, c.email as 'E-mail',c.senha,c.confirmarSenha as 'Confirmar Senha', cl.nomeClasse as 'Classe', m.voto 'Voto' From cadastro as c join classe as cl on c.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica;
 
--- Somar votos da classe suporte
-select sum(voto) as 'Total de Votos da classe Suporte'from metrica join classe on fkmetrica = idmetrica where nomeClasse = 'Suporte';
+-- metrica gráfica
+select c.nomeClasse as 'Classe', count(*) as votos from usuario u join classe c on u.fkClasse = c.idClasse group by c.nomeClasse;
 
--- Somar votos da classe Dano
-select sum(voto) as 'Total de Votos da classe Dano'from metrica join classe on fkmetrica = idmetrica where nomeClasse = 'dano';
-
--- Somar votos da classe Tanque
-select sum(voto) as 'Total de Votos da classe Tanque'from metrica join classe on fkmetrica = idmetrica where nomeClasse = 'tanque';
 
 
