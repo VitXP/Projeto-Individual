884b4dbf8b7bac8201ef6a8ec7b34b9c76c31d52
vitXP <vitor.pereira@sptech.school>
2022-11-21 01:19:45 -0300

    Alteração do background e atualização do script-tabela

--- Site/src/database/script-tabelas.sql
+++ Site/src/database/script-tabelas.sql
@@ -5,18 +5,18 @@ create database hero;
 use hero;
 
 -- Criar tabelas
-create table Metrica (
-idMetrica int primary key auto_increment,
-voto int
+create table Funcao (
+idFuncao int primary key auto_increment,
+descricao varchar(200)
 );
 
 
 create Table Classe (
 idClasse int auto_increment,
-primary key (idClasse,fkMetrica),
+primary key (idClasse,fkFuncao),
 nomeClasse varchar(7), constraint chknomeClasse check (nomeClasse in ('suporte','dano','tanque')),
-fkMetrica int,
-foreign key (fkMetrica) references metrica (idMetrica)
+fkFuncao int,
+foreign key (fkFuncao) references funcao (idFuncao)
 );
 
 
@@ -33,10 +33,10 @@ foreign key (fkClasse) references classe (idClasse)
 
 -- Inserir dados
 -- Inserir dados
-insert into metrica values
-(1,1),
-(2,1),
-(3,1);
+insert into Funcao values
+(1,' Heróis de suporte fortalecem seus aliados, pois são capazes de curá-los, protegê-los com escudos, aumentar seu poder de dano e imobilizar os inimigos.'),
+(2,'Heróis de dano são responsáveis por procurar, atacar e aniquilar inimigos, valendo-se de uma ampla variedade de ferramentas, habilidades e estratégias.'),
+(3,'Heróis desse tipo são especializados em proteger e dar cobertura à seus aliados, tirar o foco de inimigos e ocupar a linha de frente.');
 
 insert into classe values
 (null,'suporte',1),
@@ -48,21 +48,14 @@ insert into classe values
 -- Selecionar tabelas
 select * from usuario;
 select * from classe;
-select * from metrica;
+select * from funcao;
 
--- Selecionar cadastro, classe e metrica
-select*From usuario as c join classe as cl on c.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica;
+--  Selecionar apelido, email, senha, nomeClasse e voto das tabelas cadastro, classe.
+select  u.nome, u.email as 'E-mail',u.senha, cl.nomeClasse as 'Classe', f.descricao as 'Descrição' From usuario as u join classe as cl on u.fkclasse = cl.idclasse join funcao as f on f.idfuncao = cl.fkfuncao order by u.nome;
 
---  Selecionar apelido, email, senha, nomeClasse e voto das tabelas cadastro, classe e metrica.
-select  u.nome, u.email as 'E-mail',u.senha, cl.nomeClasse as 'Classe', m.voto 'Voto'  From usuario as u join classe as cl on u.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica;
 
--- Somar votos da classe suporte
-select sum(voto) From usuario as c join classe as cl on c.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica  where nomeClasse = 'suporte'; 
+-- metrica gráfica
+select c.nomeClasse as 'Classe', count(*) as votos from usuario u join classe c on u.fkClasse = c.idClasse group by c.nomeClasse;
 
--- Somar votos da classe Dano
-select sum(voto) From usuario as c join classe as cl on c.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica  where nomeClasse = 'dano'; 
-
--- Somar votos da classe Tanque
-select sum(voto) From usuario as c join classe as cl on c.fkclasse = cl.idclasse join metrica as m on cl.fkmetrica = m.idmetrica  where nomeClasse = 'tanque'; 
 
 
