35cb678527b0fc2e1e0741e32908d5fc13f04449
vitXP <vitor.pereira@sptech.school>
2022-11-24 21:05:46 -0300

    Otimização do cógido do gráfico

--- Site/public/js/grafico.js
+++ Site/public/js/grafico.js
@@ -1,7 +1,7 @@
  
     let proximaAtualizacao;
         
-    window.onload = obterDadosGrafico(1);
+    window.onload = obterDadosGrafico();
 
 
     function alterarTitulo(fkClasse) {
@@ -124,59 +124,5 @@
             config
         );
 
-        // setTimeout(() => atualizarGrafico(fkClasse, dados, myChart), 2000);
     }
-
-
-    // Esta função *atualizarGrafico* atualiza o gráfico que foi renderizado na página,
-    // buscando a última medida inserida em tabela contendo as capturas, 
-
-    //     Se quiser alterar a busca, ajuste as regras de negócio em src/controllers
-    //     Para ajustar o "select", ajuste o comando sql em src/models
-    // function atualizarGrafico(fkClasse, dados, myChart) {
-
-    //     fetch(`/medidas/tempo-real/${fkClasse}`, { cache: 'no-store' }).then(function (response) {
-    //         if (response.ok) {
-    //             response.json().then(function (novoRegistro) {
-
-    //                 console.log(`Dados recebidos: ${JSON.stringify(novoRegistro)}`);
-    //                 console.log(`Dados atuais do gráfico:`);
-    //                 console.log(dados);
-
-
-    //                 if (novoRegistro[0].momento_grafico == dados.labels[dados.labels.length - 1]) {
-    //                     console.log("---------------------------------------------------------------")
-    //                     console.log("Como não há dados novos para captura, o gráfico não atualizará.")
-    //                     document.getElementById("avisoCaptura").innerHTML = "<i class='fa-solid fa-triangle-exclamation'></i> Foi trazido o dado mais atual capturado pelo sensor. <br> Como não há dados novos a exibir, o gráfico não atualizará."
-    //                     console.log("Horário do novo dado capturado:")
-    //                     console.log(novoRegistro[0].momento_grafico)
-    //                     console.log("Horário do último dado capturado:")
-    //                     console.log(dados.labels[dados.labels.length - 1])
-    //                     console.log("---------------------------------------------------------------")
-    //                 } else {
-    //                     // tirando e colocando valores no gráfico
-    //                     dados.labels.shift(); // apagar o primeiro
-    //                     dados.labels.push(novoRegistro[0].momento_grafico); // incluir um novo momento
-
-    //                     dados.datasets[0].data.shift();  // apagar o primeiro de umidade
-    //                     dados.datasets[0].data.push(novoRegistro[0].VOTOS); // incluir uma nova medida de umidade
-
-    //                     myChart.update();
-    //                 }
-
-                //     // Altere aqui o valor em ms se quiser que o gráfico atualize mais rápido ou mais devagar
-                //     proximaAtualizacao = setTimeout(() => atualizarGrafico(fkClasse, dados, myChart), 2000);
-                // });
-            // } else {
-            //     console.error('Nenhum dado encontrado ou erro na API');
-            //     // Altere aqui o valor em ms se quiser que o gráfico atualize mais rápido ou mais devagar
-            //     proximaAtualizacao = setTimeout(() => atualizarGrafico(fkClasse, dados, myChart), 2000);
-            // }
-    // //   })
-    //         .catch(function (error) {
-    //             console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
-    //         });
-
-    // }
-
 
